87968c37e95557970dc25c42b25a0c31a0594b26
Anastasia Buglakova <115065638+AnastasiaBuglakova@users.noreply.github.com>
2023-10-20 13:34:38 +0300

    Update constants.py after comments

--- constants.py
+++ constants.py
@@ -3,5 +3,17 @@ MIN_DUTY_TO_TAKE = 30
 DUTY_TO_TAKE_PERCENT = 1.5 / 100
 BASE = 50
 WEALTH_LIMIT = 5_000_000
-WEALTH_PERCENT = 10/100
-PERCENT_FOR_3RD_OPERATION = 3/100
+WEALTH_PERCENT = 10 / 100
+PERCENT_FOR_3RD_OPERATION = 3 / 100
+DELAY = 1000
+
+texts = {'enter card num': 'Введите номер карты',
+         'enter pin': 'Введите пин-код',
+         'choose operation': 'Выберите операцию: Снять, пополнить, выйти',
+         'enter sum take': 'Введите сумму снятия:',
+         'base problem': f"Сумма должна быть кратна {BASE}",
+         'enter sum push': 'Введите сумму, которую хотите внести на счет:',
+         'wrong data': 'Введены неверные номер карты и/или пин-код',
+         'after taking': ['Выдано', 'комиссия', 'на счете'],
+         'not enough money': 'Недостаточно средств на счете',
+         'after pushing': ['Зачислено', 'комиссия', 'на счете']}
